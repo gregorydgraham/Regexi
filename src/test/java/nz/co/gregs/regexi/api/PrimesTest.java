02200647f5b3e6e49a996ebb0ea82b4973eaac02
Gregory Graham <gregory@gregs.co.nz>
2024-11-07 18:00:35 +1300

    Fixed JavaDocs

--- src/test/java/nz/co/gregs/regexi/api/PrimesTest.java
+++ src/test/java/nz/co/gregs/regexi/api/PrimesTest.java
@@ -1,6 +1,5 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * @author gregorygraham
  */
 package nz.co.gregs.regexi.api;
 
